c8496ffd7139692aef43e7db54292091eda3c041
Dorian <dorian.keurinck@decathlon.com>
2021-03-30 10:40:06 +0200

    fix : pb with struct

--- libs/libs.h
+++ libs/libs.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct 
 {
@@ -36,7 +37,8 @@ typedef struct
 
 typedef struct 
 {
-    char* nom;
+    cours cours;
+    time_t date;
     groupe groupe;
     presence listePresence[];
 
